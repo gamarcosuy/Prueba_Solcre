d6422c3e55079e65a5e14ff4b940a7a8891d4e0f
Marcos Antonio Alegrette De León <91229946+gamarcosuy@users.noreply.github.com>
2022-08-14 01:51:26 -0300

    Contador con su logica separada

---
+++ contador.js
@@ -0,0 +1,75 @@
+let horas = 12;
+let minutos = 0;
+let segundos = 0;
+
+//Segundos
+verSegundos();
+
+function verSegundos(){
+    let segundosTexto;
+
+    if(segundos < 0){
+        segundos = 59;
+    }
+
+    if(segundos < 10){
+        segundosTexto = `0${segundos}`
+    }else{
+        segundosTexto = segundos;
+    }
+    segundosTxt = document.querySelector('.segundos')
+    segundosTxt.innerHTML = segundosTexto;
+    segundos--;
+
+    verMinutos(segundos)
+}
+setInterval(verSegundos, 1000)
+
+//Minutos
+
+
+function verMinutos(segundos){
+    let minutosTexto;
+
+    if(segundos == -1 && minutos !== 0){
+        setTimeout(()=>{
+            minutos--
+        }, 500)
+    }else if(segundos == -1 && minutos == 0 ){
+        setTimeout(()=>{
+            minutos = 59
+        }, 500)
+    }
+
+    if(minutos < 10){
+        minutosTexto = `0${minutos}`
+    }else{
+        minutosTexto = minutos;
+    }
+    minutosTxt = document.querySelector('.minutos')
+    minutosTxt.innerHTML = minutosTexto;
+
+    verHoras(segundos,minutos)
+}
+
+function verHoras(segundos,minutos){
+    let horasTexto;
+
+    if(segundos == -1 && minutos !== 0 && horas !==0){
+        setTimeout(()=>{
+            horas--
+        }, 500)
+    }else if(segundos == -1 && minutos == 0 && horas ==0){
+        setTimeout(()=>{
+            horas = 23
+        }, 500)
+    }
+
+    if(horas < 10){
+        horasTexto = `0${horas}`
+    }else{
+        horasTexto = horas;
+    }
+    horasTxt = document.querySelector('.horas')
+    horasTxt.innerHTML = horasTexto;
+}
